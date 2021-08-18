5bb7ed13564b09d08e9bb7a437cbd2dc367f0633
unknown <mobinh264@gmail.com>
2021-08-19 02:18:16 +0600

    [array-slices] done

---
+++ array-slices.js
@@ -0,0 +1,22 @@
+const numbers =  [22, 2, 58, 54, 55, 59, 71, 655 ];
+// const second = [ 411, 15, 596, 21, 11, 52, 64 ]
+// slice
+const numberSlice = numbers.slice( 3, 6 );
+// console.log(numberSlice)
+// console.log(numbers) **[ main array can not change ;
+
+// splice to remove elment to existing Array
+
+// const numbeSpliced = numbers.splice( 4, 2 );
+// console.log(numberSlice);
+
+
+// const numbeSpliced = numbers.splice( 4, 0 ); **mane kno element ke delete krbo na
+
+
+
+// const numberSpliced = numbers.splice( 4, 2, 45, 47, 45, 36, 71, 91, 34 ); 
+//**mane ai j prothom (4) ta hoche index number and second (2) ta holo deleter jonnow number koi ta ar porer ortat third ta (45) thake sob guli add hobe array te as a number hisebe 
+
+// console.log(numberSpliced)
+// console.log(numbers)
