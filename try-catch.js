0f597d4947bfa6b90fba51c47d496018b6bc9eb1
unknown <mobinh264@gmail.com>
2021-08-19 14:41:55 +0600

    try-catch also one more thing finally

---
+++ try-catch.js
@@ -0,0 +1,57 @@
+// typeError can't we assign const value Again
+/* 
+const name = "Ami";
+name = "tmi";
+console.log(name);
+ */
+
+
+// SyntaxError grammerticaly Error
+/*
+for( let i = 0; i < 10 i++){
+
+}
+
+// 
+
+ */
+
+// missing initializer const 
+/* 
+const student;
+console.log(student.name);
+ */
+
+
+// undefined type Error
+/*
+ const student = undefined;
+console.log(student.name);
+*/
+
+// function typeError
+/* 
+const student = undefined;
+console.log(student());
+ */
+
+// Using Try Catch and find out Problem
+
+
+// Some More Error throw , throw now 
+/* 
+const myName = "Maruf";
+let myAnoterName = "Mobin";
+try{
+    myName = "Mobin";
+}
+catch(error){
+    console.log(error);
+}
+finally{
+    
+}
+myAnoterName = "Hussian";
+
+console.log(myAnoterName)
+ */
