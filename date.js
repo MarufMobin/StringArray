38587d78c89521055a982a5b65a61a83e9dc079d
unknown <mobinh264@gmail.com>
2021-08-19 15:48:58 +0600

    data related working

---
+++ date.js
@@ -0,0 +1,13 @@
+/* const myFavDate = new Date('1971-12-16');
+// console.log(myFavDate);
+
+const anoterDate = new Date(1971, 3, 26, 11, 50, 40, 80);
+
+// console.log(anoterDate);
+
+if( myFavDate.getTime() < anoterDate.getTime()){
+    console.log('Favorate Date Earlier');
+}
+ */
+
+for (let i==0; i<10;i++){}
